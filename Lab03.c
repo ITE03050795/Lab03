a3d28193cbc0fe8410ad0bf185f5315461c500f9
03050795 <ite@AA702.(none)>
2016-09-19 17:19:14 +0800

    9/19

---
+++ Lab03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+	printf("Please enter ten numbers:\n");
+	scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
+	printf("Your numbers is:\n");
+	printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+	return 0;
+}
